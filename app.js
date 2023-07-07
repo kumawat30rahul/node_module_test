9fae0ccf2e6c85ca96e3dd8dc96cef79f009fba2
Rahul Kumawat <kumawat30rahul@gmail.com>
2023-07-07 15:12:24 +0530

    first commit

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ dotenv.config();
 
 //middlewares
 app.use(express.json());
-app.set('views', `${__dirname}/views`);
-app.use(express.static(`${__dirname}/public`));
+// app.set('views', `${__dirname}/views`);
+// app.use(express.static(`${__dirname}/public`));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
